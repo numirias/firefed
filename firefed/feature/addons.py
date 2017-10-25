f8b83fc7976768c2b9d92ab35297aa17637eeb92
numirias <numirias@users.noreply.github.com>
2017-10-25 17:15:25 +0200

    Add signature and visibility status to addon feature

--- firefed/feature/addons.py
+++ firefed/feature/addons.py
@@ -4,12 +4,27 @@ from output import good, bad, info
 from tabulate import tabulate
 
 
+def signed_state(num):
+    # See constants defined in [1]
+    states = {
+        -2: 'broken',
+        -1: 'unknown',
+        0: 'missing',
+        1: 'preliminary',
+        2: 'signed',
+        3: 'system',
+        4: 'privileged'
+    }
+    text = states[num]
+    return good(text) if num > 0 else bad(text)
+
+
 class Addons(Feature):
     def run(self, args):
         with open(self.profile_path('extensions.json')) as f:
             addons = json.load(f)['addons']
-        info(('%d addons found. (%d active)\n' %
-              (len(addons), sum(addon['active'] for addon in addons))))
+        info('%d addons found. (%d active)\n' %
+              (len(addons), sum(addon['active'] for addon in addons)))
         if args.summarize:
             return
         addons.sort(key=lambda x: not x['active'])
@@ -19,5 +34,13 @@ class Addons(Feature):
             id_ = addon['id']
             version = addon['version']
             active = good('enabled') if addon['active'] else bad('disabled')
-            table.append([name, id_, version, active])
-        info(tabulate(table, headers=['Name', 'ID', 'Version', 'Status']))
+            try:
+                signed = signed_state(addon['signedState'])
+            except KeyError:
+                signed = '(unspecified)'
+            visible = good('true') if addon['visible'] else bad('false')
+            table.append([name, id_, version, active, signed, visible])
+        info(tabulate(table, headers=['Name', 'ID', 'Version', 'Status', 'Signature', 'Visible']))
+
+
+# [1]: https://dxr.mozilla.org/mozilla-central/rev/967c95cee709756596860ed2a3e6ac06ea3a053f/toolkit/mozapps/extensions/AddonManager.jsm#3495
