a96639c2394e190fc4ccf8d8e7037a85e339f9cd
numirias <numirias@users.noreply.github.com>
2017-11-04 04:37:06 +0100

    Add session storage to cookie feature + add lz4 loading to feature + refactorings

--- firefed/feature/cookies.py
+++ firefed/feature/cookies.py
@@ -1,26 +1,110 @@
-from feature import Feature, SqliteTableFeature
-from output import info
+import json
+import lz4
+import os
+from fnmatch import fnmatch
+import csv
+import sys
+
+from feature import Feature
+from output import info, error
 
 
-class Cookies(SqliteTableFeature, Feature):
+class Cookie:
 
-    db_file = 'cookies.sqlite'
-    table_name = 'moz_cookies'
-    num_text = '%s cookies found.'
-    fields = [
-        'name', 'value', 'baseDomain', 'host', 'path', 'isSecure', 'isHttpOnly'
-    ]
+    _column_map = {
+        'name': 'name',
+        'value': 'value',
+        'host': 'host',
+        'path': 'path',
+        'isSecure': 'secure',
+        'isHttpOnly': 'http_only',
+    }
+    _fields = tuple(_column_map.values())
 
-    def process_result(self, result):
-        for name, value, base, host, path, secure, http_only in result:
-            # Re-assemble cookies based rougly on the 'Set-Cookie' format
-            cookie = '%s: %s=%s' % (base, name, value)
-            if base:
-                cookie += '; Domain=%s' % host
-            if secure:
-                cookie += '; Secure'
-            if http_only:
-                cookie += '; HttpOnly'
-            if path != '/':
-                cookie += '; Path=%s' % path
-            info(cookie)
+    def __init__(self, **kwargs):
+        for field in self._fields:
+            setattr(self, field, kwargs[field])
+
+    def __str__(self):
+        # Re-assemble cookies based rougly on the 'Set-Cookie' format
+        text = '%s=%s' % (self.name, self.value)
+        if self.host:
+            text += '; Domain=%s' % self.host
+        if self.secure:
+            text += '; Secure'
+        if self.http_only:
+            text += '; HttpOnly'
+        if self.path != '/':
+            text += '; Path=%s' % self.path
+        return text
+
+
+def session_file(key):
+    session_file_map = {
+        'recovery': os.path.join('sessionstore-backups', 'recovery.jsonlz4'),
+        'previous': os.path.join('sessionstore-backups', 'previous.jsonlz4'),
+        'sessionstore': 'sessionstore.jsonlz4',
+    }
+    try:
+        return session_file_map[key]
+    except KeyError:
+        return key
+
+
+class Cookies(Feature):
+
+    def add_arguments(parser):
+        parser.add_argument(
+            '-f',
+            '--format',
+            default='list',
+            choices=['list', 'csv'],
+            help='output format',
+        )
+        parser.add_argument(
+            '-H',
+            '--host',
+            help='glob for matching host name',
+        )
+        parser.add_argument(
+            '-s',
+            '--session-file',
+            type=session_file,
+            help='extract cookies from session file ("recovery", "previous", "sessionstore" are shortcuts for default file locations)',
+        )
+
+    def run(self):
+        if self.args.session_file:
+            try:
+                cookies = self.load_ss_cookies(self.args.session_file)
+            except FileNotFoundError as e:
+                error('File "%s" not found.' % e.filename)
+                return
+        else:
+            cookies = self.load_sqlite('cookies.sqlite', 'moz_cookies', Cookie)
+        host_pattern = self.args.host
+        if host_pattern:
+            cookies = [c for c in cookies if fnmatch(c.host, host_pattern)]
+        getattr(self, 'build_%s' % self.args.format)(cookies)
+
+    def build_list(self, cookies):
+        for cookie in cookies:
+            print(cookie)
+
+    def build_csv(self, cookies):
+        writer = csv.writer(sys.stdout)
+        writer.writerow(Cookie._fields)
+        for cookie in cookies:
+            writer.writerow((getattr(cookie, key) for key in Cookie._fields))
+
+    def load_ss_cookies(self, path):
+        data = json.loads(self.load_moz_lz4(path))
+        cookies = data['cookies']
+        return [Cookie(
+            host=cookie['host'],
+            name=cookie['name'],
+            value=cookie['value'],
+            path=cookie['path'],
+            secure=cookie['secure'] if 'secure' in cookie else False,
+            http_only=cookie['httponly'] if 'httponly' in cookie else False,
+        ) for cookie in cookies]
--- firefed/feature/feature.py
+++ firefed/feature/feature.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import json
+import lz4
 
 
 class Feature:
@@ -26,6 +27,29 @@ class Feature:
             data = json.load(f)
         return data
 
+    def load_sqlite(self, db_path, table, cls):
+        def obj_factory(cursor, row):
+            dict_ = {}
+            for idx, col in enumerate(cursor.description):
+                new_name = cls._column_map[col[0]]
+                dict_[new_name] = row[idx]
+            return cls(**dict_)
+        con = sqlite3.connect(self.profile_path(db_path))
+        con.row_factory = obj_factory
+        cursor = con.cursor()
+        result = cursor.execute('SELECT %s FROM %s' %
+                                (','.join(cls._column_map), table)).fetchall()
+        return result
+        con.close()
+        return result
+
+    def load_moz_lz4(self, path):
+        with open(self.profile_path(path), 'rb') as f:
+            if f.read(8) != b'mozLz40\0':
+                raise Exception('Not Mozilla lz4 format.')
+            data = lz4.block.decompress(f.read())
+        return data
+
     def add_arguments(parser):
         pass
 
@@ -41,8 +65,7 @@ class SqliteTableFeature:
         if self.args.summarize:
             return
         result = cursor.execute('SELECT %s FROM %s' %
-                                (','.join(self.fields),
-                                 self.table_name)).fetchall()
+                                (','.join(self.fields), self.table_name)).fetchall()
         self.process_result(result)
 
     def process_result(self, result):
