7f8cddc96770448ccf0726cb73f03e0244373658
numirias <numirias@users.noreply.github.com>
2017-11-20 03:23:50 +0100

    Add yellow output color

--- firefed/output.py
+++ firefed/output.py
@@ -1,19 +1,21 @@
 from colorama import init, Fore, Style
 
+
 init()
 
+# TODO: dependency on colorama really necessary?
 
 def error(text):
     print(Fore.RED + 'Error: %s' % text + Style.RESET_ALL)
 
-
 def info(*args, **kwargs):
     print(*args, **kwargs)
-
 
 def good(text):
     return Fore.GREEN + text + Style.RESET_ALL
 
-
 def bad(text):
     return Fore.RED + text + Style.RESET_ALL
+
+def okay(text):
+    return Fore.YELLOW + text + Style.RESET_ALL
