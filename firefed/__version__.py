53cf64b25b33d4be9ada366c8be02953ac2ca5c6
numirias <numirias@users.noreply.github.com>
2018-01-31 21:12:48 +0100

    Bump version

--- firefed/__version__.py
+++ firefed/__version__.py
@@ -1,5 +1,5 @@
 __title__ = 'firefed'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __description__ = 'A tool for Firefox profile analysis, data extraction, \
 forensics and hardening'
 __url__ = 'https://github.com/numirias/firefed'
